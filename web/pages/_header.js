674be99dd50bf63dcf666b3e94ffd1ba3bf569f5
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-02-15 08:48:00 +0100

    Ajoute Bootstrap

---
+++ web/pages/_header.js
@@ -0,0 +1,13 @@
+// Header
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <Head>
+      <title>Webtoons Gallery</title>
+      <meta name="description" content="Webtoons Gallery powered by Team PC Pour Tous" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
